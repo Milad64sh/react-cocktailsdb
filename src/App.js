489f4c4c0f080f334b0edaec00f851fdab07d346
Milad64sh <m.shalikarian@gmail.com>
2022-07-11 07:54:53 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Error from './pages/Error';
 import SingleCocktail from './pages/SingleCocktail';
+// import navbar
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path='/'>
           <Home />
