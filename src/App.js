e44278c15d212a0ea014737c9ae6384d1589e949
Milad64sh <m.shalikarian@gmail.com>
2022-07-11 07:43:18 -0700

    basic router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import SingleCocktail from './pages/SingleCocktail';
 
 export default function App() {
-  return <h1>app component</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/About'>
+          <About />
+        </Route>
+        <Route path='/cocktail/:id'>
+          <SingleCocktail />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
