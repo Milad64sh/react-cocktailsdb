3bbd8f47cc4682fb65dfdbfb2f53dfc81879fc06
Milad64sh <m.shalikarian@gmail.com>
2022-07-11 11:12:15 -0700

    single cocktail http req

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -3,5 +3,57 @@ import { useParams } from 'react-router-dom';
 
 export default function SingleCocktail() {
   const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const data = await response.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strCategory: category,
+            strAlcoholic: info,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredients: strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getCocktail();
+  }, [id]);
   return <h1>single cocktail page</h1>;
 }
